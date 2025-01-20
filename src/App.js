35d67b55c088cf178d40b4423a60c6452710dc6a
PRERNASONI2000 <167075251+PRERNASONI2000@users.noreply.github.com>
2025-01-20 17:27:32 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // For JavaScript components
-import { BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Service from './pages/services/Service';
@@ -10,32 +10,31 @@ import Login from './pages/login/Login';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 
-// import Sidebar from './components/sidebar/Sidebar';
 const App = () => {
-   const location = useLocation(); //// Get the current route location
-   const hideNavbarFooter = location.pathname === '/login';
+  const location = useLocation(); // Get the current route location
+  const hideNavbarFooter = location.pathname === '/login';
+
   return (
     <div className='home'>
-    {/* Only render Navbar if it's not the login page */}
-    {!hideNavbarFooter && <Navbar />}
-    
-    <div className='content'>
-      <div className='homeContainer'>
-      {location.pathname === '/' && <Home />}
-        <Routes>
-          {/* <Route path='/' element={<Home />} /> */}
-          <Route path='/about' element={<About />} />
-          <Route path='/services' element={<Service />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
+      {/* Render Navbar if it's not the login page */}
+      {!hideNavbarFooter && <Navbar />}
+
+      <div className='content'>
+        <div className='homeContainer'>
+          <Routes>
+            {/* Only show Home page if the path is '/' */}
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/services' element={<Service />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
       </div>
+
+      {/* Render Footer if it's not the login page */}
+      {!hideNavbarFooter && <Footer />}
     </div>
-
-    {/* Only render Footer if it's not the login page */}
-    {!hideNavbarFooter && <Footer />}
-  </div>
-
   );
 };
 
@@ -45,71 +44,4 @@ const AppWrapper = () => (
   </BrowserRouter>
 );
 
-
 export default AppWrapper;
-
-// replaces writing path="/" for the default route in the context of nested routing.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
